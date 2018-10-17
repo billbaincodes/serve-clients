1804e338b56bfdfcaee47f25429388cf2cd7d164
bill <williamandrewbain@gmail.com>
2018-10-17 15:51:20 -0600

    added request for individual items

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3333
+const cohorts = require('./cohorts.js')
+
 
 app.get('/', (req, res, next) => {
-  res.send('heyo🍄')
+  res.send(cohorts)
 })
+
+
+app.get('/:index', (req, res, next) => {
+  res.send(cohorts[req.params.index-1])
+})
+
 
 app.listen(port, () => {
   console.log(`I'm up on ye old port ${port}`)
